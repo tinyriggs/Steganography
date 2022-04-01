9864a9ff6cb7bf0f068769f78ba220854afb8589
tinyriggs <nr107984@gmail.com>
2022-04-01 16:47:35 -0400

    I think I got encoding working

--- steganography.py
+++ steganography.py
@@ -1,7 +1,11 @@
 import json
+import string
 import numpy
+import logging
 from PIL import Image
+# from matplotlib import image, pyplot
 
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 def message_to_binary(message):
     if type(message) == str:
@@ -20,29 +24,45 @@ def binary_to_message(binary):
         return string
 
 
-def get_image(image_path):
-    """Get a numpy array of an image so that one can access values[x][y]."""
-    image = Image.open(image_path, "r")
-    width, height = image.size
-    pixel_values = list(image.getdata())
-    if image.mode == "RGB":
-        channels = 3
-    elif image.mode == "L":
-        channels = 1
-    else:
-        print("Unknown mode: %s" % image.mode)
-        return None
-    pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
-    return pixel_values
+def get_image_array(image_path : string):
+    picture = Image.open(image_path)
+    return numpy.array(picture)
 
 # numpy.binary_repr(int) returns the int as binary
 # int("binary", 2) returns binary as int
 
 
-image = get_image("picture.png")
+pixel_data = get_image_array("picture.png")
+
+
+test_string = "123456789"
+
+test_binary = message_to_binary(test_string)
+
+def encode_binary_to_picture(binary, pixel_data):
+    pixel_count1 = 0
+    pixel_count2 = 0
+    binary = list(binary)
+    while len(binary) > 0:
+        holder = numpy.binary_repr(pixel_data[0][pixel_count2][pixel_count1])
+        holder = holder[:7] + binary.pop(0)
+        pixel_data[0][pixel_count2][pixel_count1] = int(holder, 2)
+        if pixel_count1 < 2:
+            pixel_count1 += 1
+        else:
+            pixel_count1 = 0
+            pixel_count2 += 1
+    return pixel_data
 
 
 
 
+# binary.pop(0)
 
+# encode_binary_to_picture(test_binary, pixel_data)
 
+# string[:7] + "Q" puts q in the 8th spot of the string
+
+encoded_data = encode_binary_to_picture(test_binary, pixel_data)
+
+Image.fromarray(encoded_data).save("finished_picture.png")
