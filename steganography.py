6ac2e74c3c013db25a53a6a24139b839eec31cda
tinyriggs <nr107984@gmail.com>
2022-04-01 01:22:07 -0400

    message to binary and back in two functions

--- steganography.py
+++ steganography.py
@@ -1,10 +1,25 @@
+import json
 
 
-def string_to_binary(message):
-    answer = "".join(format(ord(i), "08b") for i in message)
-    return answer
 
-def binary_to_string(message):
-    n = int(message, 2)
-    return n.to_bytes((n.bit_length() + 7) // 8, "big").decode()
+def message_to_binary(message):
+    if type(message) == str:
+        return "".join(format(ord(i), "08b") for i in message)
+    elif type(message) == dict:
+        return "".join(format(ord(i), "08b") for i in json.dumps(message))
+    else:
+        raise TypeError("Input type not supported")
+
+def binary_to_message(binary):
+    n = int(binary, 2)
+    string = n.to_bytes((n.bit_length() + 7) // 8, "big").decode()
+    if string[0] == "{":
+        return json.loads(string)
+    else:
+        return string
+
+test_dictionary = {"A" : "test", "B" : "Fuck", "C" : ["hello", "ass"]}
+
+
+
 
