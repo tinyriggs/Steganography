e362009db9507096fc2482fdf2c31530e897c32a
tinyriggs <nr107984@gmail.com>
2022-04-07 19:16:16 -0400

    Started work on different lengths of strings

--- stenanography_decoder.py
+++ stenanography_decoder.py
@@ -8,11 +8,13 @@ from PIL import Image
 
 pixel_data = encoder.get_image_array("finished_picture.png")
 
+
+
 def decode_picture_to_binary(pixel_data):
     answer = ""
     pixel_count1 = 0
     pixel_count2 = 0
-    while len(answer) < 72:
+    while True:
         holder = numpy.binary_repr(pixel_data[0][pixel_count2][pixel_count1])
         answer += str(holder[7])
         if pixel_count1 < 2:
@@ -21,6 +23,8 @@ def decode_picture_to_binary(pixel_data):
             pixel_count1 = 0
             pixel_count2 += 1
     return answer        
+
+
 
 message_binary = decode_picture_to_binary(pixel_data)
 
